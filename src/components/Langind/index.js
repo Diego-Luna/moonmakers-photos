317338689493ec2a3267d5f21d26ba7724d7605b
Dieg Luna <diegofcolunalopez@gmail.com>
2021-09-06 15:32:19 -0500

    Langind solo html

--- src/components/Langind/index.js
+++ src/components/Langind/index.js
@@ -1,7 +1,99 @@
 import React from "react";
 
+import BotomBlue from "../General/Buttons/BotomBlue";
+import "../../assets/styles/components/langind.css";
+
 function LangindUI() {
-  return <div>LangindUI</div>;
+  return (
+    <React.Fragment>
+      {/* intro a about */}
+      <section className="secction intro ">
+        <div>
+          <h1>
+            El hogar de tus <br /> recuerdos
+          </h1>
+          <BotomBlue url="/photos" text="Ir a MoonMakers Fotos" />
+        </div>
+      </section>
+
+      {/* tus recuerdos en toos los dispositivos */}
+      <section className="secction container ">
+        <div className="container-secction-small">
+          <h2>
+            Tus recuerdos en <br /> todos los <br /> dispositivos
+          </h2>
+          <p>
+            Crea copias de seguridad de tus fotos y videos en Google Fotos y
+            disfruta de ellos en cualquier dispositivo
+          </p>
+        </div>
+        <ficture className="container-secction-big img-container">
+          {/* imagen de telefonos a computadoras */}
+        </ficture>
+      </section>
+
+      {/* Encuentra fotos mas rapido */}
+      <section className="secction container">
+        <div className="text-center">
+          <ficture className="img-container">{/* logo de busqueda */}</ficture>
+          <h2>Encuentra las fotos mas rapido</h2>
+          <p>
+            Tus fotos se organizan automaticamente y están <br /> disponibles
+            para busquedas para que las encuentres <br /> facilmente
+          </p>
+          <ficture className="img-container">
+            {/* imagen de ejemplo de computadora */}
+          </ficture>
+        </div>
+      </section>
+
+      {/* Guarda y comparte fácilmente lo que realmente te importa */}
+      <section className="secction container">
+        <div>
+          <ficture className="img-container">
+            {/* logo de compartir en familia */}
+          </ficture>
+
+          <h2 className="text-center">
+            Guarda y comparte facilmente <br /> lo que realmente te importa
+          </h2>
+
+          <p>Comparte fotos y álbumes con amigos y familiares.</p>
+
+          <ficture className="img-container">{/* compartir fotos */}</ficture>
+        </div>
+      </section>
+
+      {/* Edita tus recuerdos para darles vida */}
+      <section className="secction container">
+        <div className="container-secction-small">
+          <h2>
+            Edita tus <br /> recuerdos para <br /> darles vida
+          </h2>
+          <p>
+            Da al momento perfecto el acabado perfecto con las herramientas de
+            edición y los filtros inteligentes
+          </p>
+        </div>
+        <ficture className="container-secction-big img-container">
+          {/* imagen de ejemplo de computadora */}
+        </ficture>
+      </section>
+
+      {/* Saca el máximo partido de tus recuerdos con Google Fotos */}
+      <section className="secction container">
+        <div className="text-center">
+          <ficture className="img-container">
+            {/* Logo de MoonMakers Photos */}
+          </ficture>
+          <h2>
+            Saca el máximo partido de tus recuerdos <br /> con Google Fotos
+          </h2>
+          <BotomBlue url="/photos" text="Ir a MoonMakers Fotos" />
+        </div>
+      </section>
+    </React.Fragment>
+  );
 }
 
 export default LangindUI;
