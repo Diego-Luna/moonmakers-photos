7c5c226fe654b6d0da195b2f031d2fb92fed961e
Dieg Luna <diegofcolunalopez@gmail.com>
2021-09-05 19:53:13 -0500

    add search state

--- src/State/index.js
+++ src/State/index.js
@@ -5,66 +5,24 @@ const StateContex = createContext();
 
 // creamos el estado de nuestra aplicacion
 function StateProvider(props) {
-  // para crear un estado en una funciona, con react hooks
-  // const [searchValue, setSearchValue] = React.useState("");
+  // estado de Search
+  const [search, setSearch] = React.useState({
+    value: "",
+    cargando: false,
+    error: "",
+  });
 
-  // creamos un nuevo estado para nuestro modal
-  // const [openModal, setOpenModal] = React.useState(false);
+  const CambiarValorSearch = (value) => {
+    setSearch({
+      value: value,
+      cargando: false,
+      error: "",
+    });
 
-  // const completesTareas = tareas.filter(
-  //   (tareas) => tareas.completed === true
-  // ).length;
-
-  // const totalTareas = tareas.length;
-
-  // const filterTareas = tareas.filter((t) => {
-  //   return t.text.toLowerCase().includes(searchValue.toLowerCase());
-  // });
-
-  // const addTarea = (text) => {
-  //   // buscamos la tarea
-  //   const newTareas = [...tareas];
-
-  //   newTareas.push({ completed: false, text });
-
-  //   saveTareas(newTareas);
-  // };
-
-  // const completeTarea = (text) => {
-  //   // buscamos la tarea
-  //   const tareaIndex = tareas.findIndex((t) => t.text === text);
-  //   const newTareas = [...tareas];
-
-  //   newTareas[tareaIndex].completed = true;
-
-  //   saveTareas(newTareas);
-  // };
-
-  // function deleteTarea(text) {
-  //   const newTareas = tareas.filter((t) => t.text !== text);
-  //   saveTareas(newTareas);
-  // }
+  };
 
   return (
-    <StateContex.Provider
-      value={
-        {
-          // loading,
-          // error,
-          // tareas,
-          // totalTareas,
-          // searchValue,
-          // filterTareas,
-          // completesTareas,
-          // openModal,
-          // setOpenModal,
-          // addTarea,
-          // deleteTarea,
-          // completeTarea,
-          // setSearchValue,
-        }
-      }
-    >
+    <StateContex.Provider value={{ search, CambiarValorSearch }}>
       {props.children}
     </StateContex.Provider>
   );
--- src/components/Menu/Search.js
+++ src/components/Menu/Search.js
@@ -1,18 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
+// importamos neustro contexto
+import { StateContex } from "../../State";
 
 import LogoHelp from "../../assets/icons/help.png";
 import LogoUpload from "../../assets/icons/upload.png";
 import LogoSearch from "../../assets/icons/search.png";
 
 function Search() {
+  //  pasamos a nuestro react hook el contexto de nuestra aplicacion: ""
+  const { search, CambiarValorSearch } = React.useContext(StateContex);
+
+  const onSearchValueChange = (event) => {
+    console.log(event.target.value);
+
+    CambiarValorSearch(event.target.value);
+  };
+
   return (
     <div className="search-container">
       <div className="search-container_input">
-        <div className="icon-container icon-searh" >
+        <div className="icon-container icon-searh">
           <img src={LogoSearch} alt="logo de busqueda" />
         </div>{" "}
-        <input type="text" />
+        <input
+          placeholder="Buscar fotos"
+          type="text"
+          value={search.value}
+          onChange={onSearchValueChange}
+        />
       </div>
       <button className="icon-container">
         <img src={LogoUpload} alt="Logo de Upload" />
