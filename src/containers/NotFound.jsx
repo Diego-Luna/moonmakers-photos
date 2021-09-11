3caac9716fb5ba06de601aa52f30ea4aa717ece5
Dieg Luna <diegofcolunalopez@gmail.com>
2021-09-11 13:46:11 -0500

    add not found

--- src/containers/NotFound.jsx
+++ src/containers/NotFound.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+
+import LogoMoonMakers from "../assets/img/MoonMakers.png";
+import Robot from "../assets/img/robot.png";
+
+import "../assets/styles/container/NotFound.css";
 
 function NotFound() {
   return (
-    <div>
-      Not Found
-    </div>
-  )
+    <section className="center-all height-vh">
+      <div className="notfound-container">
+        <div>
+          <Link to="/" >
+            <div className="img-container-notfound">
+              <img src={LogoMoonMakers} alt="" />
+            </div>
+          </Link>
+
+          <p style={{ color: "#777" }}>
+            <b style={{ color: "black" }}>404</b>. Se trata de un error.
+          </p>
+          <p style={{ color: "#222" }}>
+            No se ha encontrado la URL solicitada en este servidor.
+          </p>
+          <p style={{ color: "#777" }}>
+            Esa es toda la información de la que disponemos.
+          </p>
+        </div>
+        <div>
+          <img src={Robot} alt="" />
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default NotFound
+export default NotFound;
