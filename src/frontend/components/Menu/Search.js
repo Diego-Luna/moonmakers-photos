6e6eade08bc83d80b52fb216453d54dde384d507
Dieg Luna <diegofcolunalopez@gmail.com>
2021-09-18 20:32:17 -0500

    add input de search

--- src/frontend/components/Menu/Search.js
+++ src/frontend/components/Menu/Search.js
@@ -7,14 +7,35 @@ import LogoHelp from "../../assets/icons/help.png";
 import LogoUpload from "../../assets/icons/upload.png";
 import LogoSearch from "../../assets/icons/search.png";
 
+// span
+import "../../assets/styles/components/search.css";
+
 function Search() {
   //  pasamos a nuestro react hook el contexto de nuestra aplicacion: ""
-  const { search, CambiarValorSearch } = React.useContext(StateContex);
+  const { search, inputSubir, CambioInputSubir, CambiarValorSearch } =
+    React.useContext(StateContex);
 
   const onSearchValueChange = (event) => {
     console.log(event.target.value);
 
     CambiarValorSearch(event.target.value);
+  };
+
+  const onInputChangeValue = (event) => {
+    CambioInputSubir(event.target.value);
+
+    // console.log(event);
+    // event.preventDefault();
+    // document.formulario1.submit();
+    // document.formulario1.submit.preventDefault;
+    // event.preventDefault();
+  };
+
+  const onSubmit = (event) => {
+    console.log("event");
+    console.log(event);
+    // cuando apretemos el boton, evita que se recarge la pagina
+    event.preventDefault();
   };
 
   return (
@@ -32,6 +53,9 @@ function Search() {
       </div>
       <button className="icon-container">
         <img src={LogoUpload} alt="Logo de Upload" />
+        <div class="file-select" id="src-file1">
+          <input onChange={onInputChangeValue} type="file" name="file" />
+        </div>
       </button>
       <button className="icon-container">
         <img src={LogoHelp} alt="Logo de Help" />
